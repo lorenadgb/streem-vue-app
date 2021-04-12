9d7438cdc742beb0327430098a8bc8124becd8dd
Lorena Bento <lorenadgb@gmail.com>
2021-04-12 19:59:20 +1000

    Add footer component

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,15 @@
+<template>
+  <footer>
+    <p>Copyright &copy; {{ currentYear }} </p>
+  </footer>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      currentYear: new Date().getFullYear(),
+    };
+  }
+};
+</script>
